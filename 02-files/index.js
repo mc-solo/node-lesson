e89cf8b85129b50bbeac0f85af3fd3129fc2ad50
wondwoesn <revatoel@gmail.com>
2024-06-06 11:09:17 +0300

    uncaught errors

--- 02-files/index.js
+++ 02-files/index.js
@@ -2,11 +2,17 @@ const { error } = require("console");
 const fs = require("fs");
 
 // always remember to use the below 2 parameters - path, encoding and error handling function
-const text = fs.readFile("./files/me.txt", "utf-8", (error, data) => {
-  if (error) {
+const text = fs.readFile("./files/me.txt", "utf8", (err, data) => {
+  if (err) {
     console.log("Oops! Something went wrong");
   }
   console.log(data);
 });
-
 console.log(text);
+
+//exit on uncaught errors
+process.on("uncaughtException", (err) => {
+  console.error("There was an uncaught error", err);
+});
+
+throw new Error("Your error");
